b18a0c90d1ba1faf1632336fea7d6b071a98ad7e
Naufal357 <naufalrozan22@gmail.com>
2024-03-01 09:32:08 +0700

    test calculateRegression

--- app/Http/Controllers/Account/TamController.php
+++ app/Http/Controllers/Account/TamController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\SurveyResponses;
 use App\Models\Survey;
 use Inertia\Inertia;
+use PhpParser\Node\Stmt\Echo_;
+use Psy\Readline\Hoa\Console;
 
 class TamController extends Controller
 {
@@ -47,8 +49,9 @@ class TamController extends Controller
         $respondentCount = $this->countRespondents($id, $responses);
         $getTAMChartData = $this->getTAMChartData($id, $responses);
         $tamSurveyResults = $this->getTAMResults($id, $responses);
-        $calculateDescriptiveStatistics = $this->getCalculateDescriptiveStatistics($id, $responses);
-
+        $calculateDescriptiveStatistics = $this->getCalculateDescriptiveStatistics($id, $responses, $this->variables);
+        $calculateRegression = $this->calculateRegression($id, $responses);
+dd($calculateRegression);
         return inertia('Account/TAM/Index', [
             'surveyTitles' => $surveyTitles,
             'survey' => $survey,
@@ -60,6 +63,44 @@ class TamController extends Controller
             'tamSurveyResults' => $tamSurveyResults,
         ])->with('currentSurveyTitle', $survey->title);
     }
+
+    private $variables = [
+        [
+            'name' => 'Perceived Ease of Use',
+            'questions' => 3,
+            'max_score' => 5,
+            'start_question' => 1,
+            'end_question' => 3,
+        ],
+        [
+            'name' => 'Perceived Usefulness',
+            'questions' => 3,
+            'max_score' => 5,
+            'start_question' => 4,
+            'end_question' => 6,
+        ],
+        [
+            'name' => 'Attitude Toward Using',
+            'questions' => 3,
+            'max_score' => 5,
+            'start_question' => 7,
+            'end_question' => 9,
+        ],
+        [
+            'name' => 'Behavioral intention to use',
+            'questions' => 3,
+            'max_score' => 5,
+            'start_question' => 10,
+            'end_question' => 12,
+        ],
+        [
+            'name' => 'Actual System use',
+            'questions' => 3,
+            'max_score' => 5,
+            'start_question' => 13,
+            'end_question' => 15,
+        ],
+    ];
 
     private function countRespondents($surveyId, $responses)
     {
@@ -89,47 +130,9 @@ class TamController extends Controller
         return response()->json($tam_data);
     }
 
-    private function getCalculateDescriptiveStatistics($surveyId, $responses)
+    private function getCalculateDescriptiveStatistics($surveyId, $responses, $variables)
     {
         $respondents = $this->countRespondents($surveyId, $responses);
-
-        $variables = [
-            [
-                'name' => 'Perceived Ease of Use',
-                'questions' => 3,
-                'max_score' => 5,
-                'start_question' => 1,
-                'end_question' => 3,
-            ],
-            [
-                'name' => 'Perceived Usefulness',
-                'questions' => 3,
-                'max_score' => 5,
-                'start_question' => 4,
-                'end_question' => 6,
-            ],
-            [
-                'name' => 'Attitude Toward Using',
-                'questions' => 3,
-                'max_score' => 5,
-                'start_question' => 7,
-                'end_question' => 9,
-            ],
-            [
-                'name' => 'Behavioral intention to use',
-                'questions' => 3,
-                'max_score' => 5,
-                'start_question' => 10,
-                'end_question' => 12,
-            ],
-            [
-                'name' => 'Actual System use',
-                'questions' => 3,
-                'max_score' => 5,
-                'start_question' => 13,
-                'end_question' => 15,
-            ],
-        ];
 
         $descriptiveStatistics = [];
 
@@ -164,6 +167,64 @@ class TamController extends Controller
             ];
         }
         return $descriptiveStatistics;
+    }
+
+    private function calculateRegression($surveyId, $responses)
+    {
+        $regressionResults = []; // Inisialisasi array untuk menyimpan hasil regresi
+
+        $variables = [
+            [
+                'name' => 'Behavioral intention to use',
+                'questions' => 3,
+                'max_score' => 5,
+                'start_question' => 10,
+                'end_question' => 12,
+            ],
+            [
+                'name' => 'Actual System use',
+                'questions' => 3,
+                'max_score' => 5,
+                'start_question' => 13,
+                'end_question' => 15,
+            ],
+        ];
+
+        foreach ($variables as $variable) {
+            $regressionValues = []; // Inisialisasi array untuk menyimpan nilai regresi untuk setiap variabel
+
+            foreach ($responses as $response) {
+                echo $variable['name'] . '<br>';
+                $responseData = json_decode($response->response_data, true);
+                $total_responses = 0;
+
+                for ($i = $variable['start_question']; $i <= $variable['end_question']; $i++) {
+                    $questionKey = 'tam' . $i;
+                    if (isset($responseData['tam'][$questionKey])) {
+                        $total_responses += $responseData['tam'][$questionKey];
+                        echo $responseData['tam'][$questionKey] . '<br>';
+                    }
+                }
+                echo $total_responses . '<br>';
+
+                $sum_sk = $variable['questions'] * $variable['max_score'];
+                $value = 0;
+
+                // Menghitung nilai regresi jika sum_sk tidak sama dengan 0
+                if ($sum_sk != 0) {
+                    $value = ($total_responses / $sum_sk) * 100;
+                    echo 'value : ' . $value . '<br>';
+                }
+
+                // Menyimpan nilai regresi dalam array regressionValues
+                $regressionValues[$variable['name']] = number_format($value, 2) . '%';
+                echo $regressionValues[$variable['name']] . '<br>'. '<br>';
+            }
+
+            $regressionResults[] = $regressionValues; // Tambahkan nilai regresi untuk variabel ke dalam array hasil
+        }
+
+        return $regressionResults; // Mengembalikan hasil regresi
     }
 
     private function getTamResults($id, $responses)
